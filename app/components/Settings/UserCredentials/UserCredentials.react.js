071aed99c16ed48134cc0885044d7e904f66c497
sobo <sobolevski.a@gmail.com>
2016-06-08 23:34:31 -0400

    :wrench: use the dialog window for database path input

--- app/components/Settings/UserCredentials/UserCredentials.react.js
+++ app/components/Settings/UserCredentials/UserCredentials.react.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import styles from './UserCredentials.css';
 import classnames from 'classnames';
 import {ENGINES} from '../Constants/SupportedEngines.react';
+const {dialog} = require('electron').remote;
 
 /*
 	Displays and alters user inputs for `configuration`
@@ -24,6 +25,16 @@ export default class UserCredentials extends Component {
 	render() {
 		const {configuration, merge} = this.props;
 
+		function getInputType (credential) {
+			if (credential === 'password') {
+				return 'password';
+			} else if (credential === 'databasePath') {
+				return 'text';
+			} else {
+				return 'text';
+			}
+		}
+
 		function getPlaceholder(credential) {
 			if (credential === 'portNumber') {
 				return 'local port number';
@@ -34,15 +45,28 @@ export default class UserCredentials extends Component {
 			}
 		}
 
+		function getOnClick(credential) {
+			return () => {
+				if (credential === 'databasePath') {
+					dialog.showOpenDialog({
+						properties: ['openFile', 'openDirectory']
+					}, (paths) => {
+						merge({
+							[credential]: paths[0]
+						});
+					});
+				}
+			};
+		}
+
 		let inputs = USER_CREDENTIALS[configuration.get('engine')].map(credential => (
 			<input
 				placeholder={getPlaceholder(credential)}
-				type={
-					credential === 'password' ? 'password' : 'text'
-				}
+				type={getInputType(credential)}
 				onChange={e => (
 					merge({[credential]: e.target.value})
 				)}
+				onClick={getOnClick(credential)}
 				value={this.props.configuration.get(credential)}
 			/>
 		));
