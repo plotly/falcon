7bca6a991270f4e5ede8f2fe3bf9d6095ce0da2e
sobo <sobolevski.a@gmail.com>
2016-06-13 14:34:22 -0400

    :wrench: move out input support functions out of render

--- app/components/Settings/UserCredentials/UserCredentials.react.js
+++ app/components/Settings/UserCredentials/UserCredentials.react.js
@@ -20,54 +20,61 @@ const USER_CREDENTIALS = {
 export default class UserCredentials extends Component {
     constructor(props) {
         super(props);
+		this.getPlaceholder = this.getPlaceholder.bind(this);
+		this.getInputType = this.getInputType.bind(this);
+		this.getOnClick = this.getOnClick.bind(this);
     }
+
+	getInputType (credential) {
+		if (credential === 'password') {
+			return 'password';
+		} else {
+			return 'text';
+		}
+	}
+
+	getPlaceholder(credential) {
+		if (credential === 'portNumber') {
+			return 'local port number';
+		} else if (credential === 'databasePath') {
+			return 'path to database';
+		} else {
+			return credential;
+		}
+	}
+
+	getOnClick(credential) {
+		return () => {
+			if (credential === 'databasePath') {
+				dialog.showOpenDialog({
+					properties: ['openFile', 'openDirectory']
+				}, (paths) => {
+					const lastItemIndicator = paths[0].split('/').length - 1;
+					this.props.merge({
+						[credential]: paths[0],
+						/*
+							set the username to database filename such that
+							it appears in the logs for the user to notice
+						*/
+						'username': paths[0].split('/')[lastItemIndicator]
+					});
+				});
+			}
+		};
+	}
 
 	render() {
 		const {configuration, merge} = this.props;
 
-		function getInputType (credential) {
-			if (credential === 'password') {
-				return 'password';
-			} else if (credential === 'databasePath') {
-				return 'text';
-			} else {
-				return 'text';
-			}
-		}
-
-		function getPlaceholder(credential) {
-			if (credential === 'portNumber') {
-				return 'local port number';
-			} else if (credential === 'databasePath') {
-				return 'path to database';
-			} else {
-				return credential;
-			}
-		}
-
-		function getOnClick(credential) {
-			return () => {
-				if (credential === 'databasePath') {
-					dialog.showOpenDialog({
-						properties: ['openFile', 'openDirectory']
-					}, (paths) => {
-						merge({
-							[credential]: paths[0]
-						});
-					});
-				}
-			};
-		}
-
 		let inputs = USER_CREDENTIALS[configuration.get('engine')].map(credential => (
 			<input
-				placeholder={getPlaceholder(credential)}
-				type={getInputType(credential)}
+				placeholder={this.getPlaceholder(credential)}
+				type={this.getInputType(credential)}
 				onChange={e => (
 					merge({[credential]: e.target.value})
 				)}
-				onClick={getOnClick(credential)}
-				value={this.props.configuration.get(credential)}
+				onClick={this.getOnClick(credential)}
+				value={configuration.get(credential)}
 			/>
 		));
 
