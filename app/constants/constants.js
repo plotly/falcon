9a64aeed92c8454e9980eb753957b4178f2e19a2
sobo <sobolevski.a@gmail.com>
2016-06-20 21:59:50 -0400

    :book: move more constants into constants.js

--- app/constants/constants.js
+++ app/constants/constants.js
@@ -13,3 +13,19 @@ export const APP_STATUS_CONSTANTS = {
     CONNECTING: 'CONNECTING',
     DISCONNECTED: 'DISCONNECTED'
 };
+
+export const USER_INPUT_FIELDS = {
+    [ENGINES.MYSQL]: ['username', 'password', 'host', 'port'],
+    [ENGINES.MARIADB]: ['username', 'password', 'host', 'port'],
+	[ENGINES.MSSQL]: ['username', 'password', 'host', 'port'],
+    [ENGINES.POSTGRES]: ['username', 'password', 'host', 'port', 'database'],
+    [ENGINES.SQLITE]: ['storage']
+};
+
+export const BUTTON_MESSAGE = {
+    INITIALIZED: 'connect',
+    ERROR: 'try again',
+    CONNECTED: 'connected',
+    CONNECTING: 'connecting...',
+    DISCONNECTED: 'connect'
+};
