465836d237e939e70077e4b985f7b8305bd78c80
sobo <sobolevski.a@gmail.com>
2016-06-23 16:53:24 -0400

    :bug: stop warnings about 'null' in input fields

--- app/reducers/configuration.js
+++ app/reducers/configuration.js
@@ -3,13 +3,13 @@ import {DIALECTS} from '../constants/constants';
 import Immutable from 'immutable';
 
 const INITIAL_STATE = Immutable.Map({
-    username: null,
-    password: null,
-    database: null,
+    username: '',
+    password: '',
+    database: '',
     dialect: DIALECTS.MYSQL,
-    port: null,
-    storage: null,
-    host: null
+    port: '',
+    storage: '',
+    host: ''
 });
 
 export default function configuration(state = INITIAL_STATE, action) {
