71f0144b91e7bb45a3c847b114c4e131ea55c15c
sobo <sobolevski.a@gmail.com>
2016-07-06 20:03:40 -0400

    :zap: old files

--- app/components/Configuration.react.js
+++ app/components/Configuration.react.js
@@ -1,42 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import * as styles from './Configuration.css';
 import Settings from './Settings/Settings.react';
-import Monitor from './Monitor/Monitor.react';
 
-const TABS = {
-    SETTINGS: 'SETTINGS',
-    MONITOR: 'MONITOR'
-};
 
 export default class Configuration extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            query: '',
-            currentTab: TABS.SETTINGS
-        };
     }
 
     render() {
-
-        let content;
-        if (this.state.currentTab === TABS.SETTINGS) {
-            content = <Settings
-                configuration={this.props.configuration}
-                configActions={this.props.configActions}
-                ipcActions={this.props.ipcActions}
-                ipc={this.props.ipc}
-                connection={this.props.connection}
-                connectionActions={this.props.connectionActions}
-            />;
-        } else {
-            content = <Monitor/>;
-        }
-
         return (
             <div>
-                {content}
+                <Settings
+                    configuration={this.props.configuration}
+                    configActions={this.props.configActions}
+                    ipcActions={this.props.ipcActions}
+                    ipc={this.props.ipc}
+                    connection={this.props.connection}
+                    connectionActions={this.props.connectionActions}
+                />;
             </div>
         );
     }
