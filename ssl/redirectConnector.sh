8555b41b40de6bee030e970c809fce9e45a65a29
sobo <sobolevski.a@gmail.com>
2016-08-01 18:40:15 -0400

    :wrench: dont need intermediate redirect from default port

--- ssl/redirectConnector.sh
+++ ssl/redirectConnector.sh
@@ -1,3 +1,1 @@
-echo "10.0.0.1\tconnector.plotly.com" >> /etc/hosts
-ifconfig lo0 10.0.0.1 alias
-echo "rdr pass on lo0 inet proto tcp from any to 10.0.0.1 port 80 -> 127.0.0.1 port 3000" | pfctl -ef -
+echo "127.0.0.1\tconnector.plot.ly" >> /etc/hosts
