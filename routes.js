9f47740c75713ee19f7f7fb380d6de451df09b0c
sobo <sobolevski.a@gmail.com>
2016-08-02 19:17:17 -0400

    :wrench: WIP for steps to accept certificate

--- routes.js
+++ routes.js
@@ -1,5 +1,35 @@
+import * as fs from 'fs';
+import restify from 'restify';
+
+function serveAcceptCerSteps(req, res) {
+  fs.readFile('./ssl/steps.html', 'utf8', function(err, file) {
+    if (err) {
+      res.send(500);
+    }
+    res.write(file);
+    res.end();
+  });
+}
+
+function serveFinished(req, res) {
+  fs.readFile('./ssl/finished.html', 'utf8', function(err, file) {
+    if (err) {
+      res.send(500);
+    }
+    res.write(file);
+    res.end();
+  });
+}
 
 export function setupRoutes(server, processMessageFunction) {
+    // serve plain html for steps to accept self-signed certificate
+    server.get(/\/ssl\/?.*/, restify.serveStatic({
+      directory: __dirname
+    }));
+
+    server.get('/steps', serveAcceptCerSteps);
+    server.get('/secure', serveFinished);
+
     server.get('/v0/connect', processMessageFunction);
     server.get('/v0/login', processMessageFunction);
     server.get('/v0/query', processMessageFunction);
