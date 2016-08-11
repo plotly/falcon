e4c137683309167eb3a8155f45c0b41ce320a645
chriddyp <chriddyp@users.noreply.github.com>
2016-08-11 12:21:22 -0400

    :dolls: More descriptive log
    The "Results received." message appears right after the `QUERY EXECUTED` and `Querying: ` message. In this context, it's a little bit unclear what "Results received" means (i.e. what is a "Result").
    I've changed "Results received" to "Query finished".

--- sequelizeManager.js
+++ sequelizeManager.js
@@ -211,7 +211,7 @@ export class SequelizeManager {
         return () => this.connection
         .query(showtables, this.setQueryType('SELECT'))
         .then(results => {
-            this.log('Results recieved.', 1);
+            this.log('Query finished', 1);
             // TODO: when switching fornt end to v1, simply send back an array
             const tablesObject = this.intoTablesArray(results).map(table => {
                 return {[table]: {}};
@@ -266,7 +266,7 @@ export class SequelizeManager {
             this.raiseError(error, callback);
         })
         .then((results) => {
-            this.log('Results received.', 1);
+            this.log('Query finished', 1);
             callback(merge(parse(results), {error: null}));
         });
 
