931c9ed0a267c7250c529b6af6133663ab7c3521
sobo <sobolevski.a@gmail.com>
2016-08-30 14:03:37 -0400

    :wrench: migrate config, connection and ipc actions

--- app/actions/connection.js
+++
@@ -1,5 +0,0 @@
-import {createAction} from 'redux-actions';
-
-export const UPDATE = 'UPDATE_CONNECTION';
-
-export const update = createAction(UPDATE);
--- app/actions/sessions.js
+++ app/actions/sessions.js
@@ -1,21 +1,38 @@
 import {createAction} from 'redux-actions';
 import Immutable from 'immutable';
 import {TASKS} from './../../backend/messageHandler';
-
 const ipcRenderer = require('electron').ipcRenderer;
 
+export const NEW_SESSION = 'NEW_SESSION';
+export const SWITCH_SESSION = 'SWITCH_SESSION';
+
+export const newSession = createAction(NEW_SESSION);
+
+export const switchSession = createAction(SWITCH_SESSION);
+
+// update calls
+export const UPDATE_CONNECTION = 'UPDATE_CONNECTION';
+export const UPDATE_CONFIGURATION = 'UPDATE_CONFIGURATION';
 export const UPDATE_IPC_STATE = 'UPDATE_IPC_STATE';
 
+export const updateConnection = createAction(UPDATE_CONNECTION);
+export const updateConfiguration = createAction(UPDATE_CONFIGURATION);
 export const updateIpcState = createAction(UPDATE_IPC_STATE);
 
+// ipc specific ->
 export function query (statement) {
-    return () => {
-        ipcSend(TASKS.QUERY, {statement});
+    return (_, getState) => {
+        const state = getState();
+        const sessionSelected = state.sessions.get('sessionSelected');
+        ipcSend(TASKS.QUERY, statement);
     };
 }
 
 export function connect (credentials) {
-    return () => {
+    console.warn('connect action dispatched');
+    return (_, getState) => {
+        const state = getState();
+        const sessionSelected = state.sessions.get('sessionSelected');
         ipcSend(
             TASKS.CONNECT_AND_SHOW_DATABASES, immutableToJS(credentials)
         );
@@ -23,10 +40,12 @@ export function connect (credentials) {
 }
 
 export function selectDatabase () {
-    return function (dispatch, getState) {
+    return (_, getState) => {
         const state = getState();
+        const sessionSelected = state.sessions.get('sessionSelected');
+        const configuration = state.sessions.getIn(['list', `${sessionSelected}`, 'configuration']);
         ipcSend(
-            TASKS.SELECT_DATABASE_AND_SHOW_TABLES, state.configuration.toJS()
+            TASKS.SELECT_DATABASE_AND_SHOW_TABLES, configuration.toJS()
         );
     };
 }
@@ -60,3 +79,4 @@ function immutableToJS(immutableThing) {
 function ipcSend(task, message = {}) {
     ipcRenderer.send('channel', {task, message});
 }
+// <- ipc specific
