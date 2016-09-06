d16d66232dc2d648a003df79888f9494801972f2
sobo <sobolevski.a@gmail.com>
2016-09-06 13:46:46 -0400

    :koala: tooltip for ids

--- app/components/SessionsManager/SessionsManager.react.js
+++ app/components/SessionsManager/SessionsManager.react.js
@@ -3,6 +3,8 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import * as styles from './SessionsManager.css';
 import classnames from 'classnames';
 import {NEW_SESSION} from '../../constants/constants';
+import ReactTooltip from 'react-tooltip';
+
 
 const LOGOS = {
     postgres: './images/postgres-logo.png',
@@ -22,6 +24,26 @@ export default class SessionsManager extends Component {
         const {sessions, sessionsActions} = this.props;
         const sessionsIds = Object.keys(sessions.get('list').toJS());
         const sessionsNB = sessionsIds.length;
+        const ID = (sessionKey) => {
+            return (
+                <span>
+            {sessions.getIn(['list', sessionKey, 'configuration', 'username'])}
+            @
+            {sessions.getIn(['list', sessionKey, 'configuration', 'host']) || 'localhost'}
+            &nbsp;
+            </span>
+        );
+        };
+
+        const tooltip = (sessionKey) => {
+            return (
+                <ReactTooltip id={sessionKey} type="warning" effect="solid">
+                    <span>
+                        {ID(sessionKey)}
+                    </span>
+                </ReactTooltip>
+            );
+        };
 
         let sessionsIcons = null;
         if (sessionsNB > 0) {
@@ -29,23 +51,11 @@ export default class SessionsManager extends Component {
             sessionsIcons = Object.keys(sessions.get('list').toJS()).map(
                 sessionKey => (
                     <div className={classnames(styles.connectionWrapper, {
-                        [styles.connectionWrapperSelected]:
-                            `${sessions.get('sessionSelected')}` === sessionKey
-                        }
-                    )}
-            >
-                        <img
-                            className={styles.connection}
-
-                            onClick={() => {
-                                sessionsActions.switchSession(sessionKey);
-                            }}
-
-                            src={LOGOS[sessions.getIn([
-                                'list', sessionKey, 'configuration', 'dialect'
-                            ])]}
-                        >
-                        </img>
+                            [styles.connectionWrapperSelected]:
+                                `${sessions.get('sessionSelected')}` === sessionKey
+                            }
+                        )}
+                    >
                         <img
                             className={styles.connectionDelete}
                             onClick={() => {
@@ -60,6 +70,30 @@ export default class SessionsManager extends Component {
                             src="./images/delete.png"
                         >
                         </img>
+
+                        <img
+                            className={styles.connection}
+
+                            onClick={() => {
+                                sessionsActions.switchSession(sessionKey);
+                            }}
+
+                            src={LOGOS[sessions.getIn([
+                                'list', sessionKey, 'configuration', 'dialect'
+                            ])]}
+                        >
+                        </img>
+
+                        <p data-tip data-for={sessionKey}
+                            className={styles.connectionIndex}
+                            onClick={() => {
+                                sessionsActions.switchSession(sessionKey);
+                            }}
+                        >
+                            {ID(sessionKey)}
+                            {tooltip(sessionKey)}
+                        </p>
+
                     </div>
                 )
         );}
