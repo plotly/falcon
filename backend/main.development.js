87a9963d10638b2c8dc7c81fc06dda8f36879fdd
sobo <sobolevski.a@gmail.com>
2016-10-14 12:33:27 -0400

    fix CI test try 2

--- backend/main.development.js
+++ backend/main.development.js
@@ -9,7 +9,6 @@ import {ipcMessageReceive,
         CHANNEL} from './messageHandler';
 import {setupHTTP, setupHTTPS, findSelfSignedCert} from './setupServers';
 import {setupMenus} from './menus';
-import {trackStartup} from './mixpanel';
 
 if (process.env.NODE_ENV === 'development') {
     require('electron-debug')();
