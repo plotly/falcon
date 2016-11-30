5c4e2208450666c2f2b96b8eaf150c8c8bf2fb8d
chriddyp <chris@plot.ly>
2016-11-30 12:31:28 -0500

    :see_no_evil: typo

--- backend/persistent/PlotlyAPI.js
+++ backend/persistent/PlotlyAPI.js
@@ -84,7 +84,7 @@ export function checkWritePermissions(fid, requestor) {
          */
         const errorMessage = (
             `Unauthenticated: Attempting to update grid ${fid} but the ` +
-            `authentication credentials for the user "${username}" do not exist.`
+            `authentication credentials for the user "${requestor}" do not exist.`
         );
         Logger.log(errorMessage, 0);
         return new Promise(function(resolve, reject) {
