136b5d0e829364d51c2457f00e64526b71193999
sobo <sobolevski.a@gmail.com>
2016-12-12 17:49:32 -0500

    :wrench: rewrite connectButton to better handle loading state

--- app/components/Settings/ConnectButton/ConnectButton.react.js
+++ app/components/Settings/ConnectButton/ConnectButton.react.js
@@ -1,8 +1,8 @@
 import React, {Component, PropTypes} from 'react';
-import ImmutablePropTypes from 'react-immutable-proptypes';
-import classnames from 'classnames';
 import * as styles from './ConnectButton.css';
-import {APP_STATUS, BUTTON_MESSAGE} from '../../../constants/constants';
+import {has} from 'ramda';
+
+const isLoading = (status) => status === 'loading';
 
 export default class ConnectButton extends Component {
 
@@ -20,30 +20,42 @@ export default class ConnectButton extends Component {
         let error = null;
 
         if (connectRequest.status >= 400 || saveConnectionsRequest.status >= 400) {
-           buttonText = 'Connect';
-           buttonClick = connect;
-           // TODO - Try out locking the home folder and verifying this.
-           let errorMessage = 'Hm... had trouble connecting.';
-           if (connectRequest.content && connectRequest.content.error) {
-               errorMessage = connectRequest.content.error;
-           } else if (saveConnectionsRequest.content && saveConnectionsRequest.content.error && saveConnectionsRequest.content.error.message) {
-               errorMessage = saveConnectionsRequest.content.error.message;
-           }
-           error = <div className={styles.errorMessage}>{errorMessage}</div>;
-       } else if (!connectRequest.status) {
             buttonText = 'Connect';
             buttonClick = connect;
-        } else if (connectRequest.status === 'loading') {
+            // TODO - Try out locking the home folder and verifying this.
+            let errorMessage = 'Hm... had trouble connecting.';
+            if (
+               has('conent', connectRequest) &&
+               has('error', connectRequest.content)
+            ) {
+               errorMessage = connectRequest.content.error;
+            } else if (
+               has('content', saveConnectionsRequest) &&
+               has('error', saveConnectionsRequest.content) &&
+               has('message', saveConnectionsRequest.content.error)
+            ) {
+               errorMessage = saveConnectionsRequest.content.error.message;
+            }
+            error = <div className={styles.errorMessage}>{errorMessage}</div>;
+        } else if (
+            isLoading(connectRequest.status) ||
+            isLoading(saveConnectionsRequest.status)
+        ) {
             buttonText = 'Connecting...';
-        } else if (connectRequest.status >= 200 && connectRequest.status < 300) {
+        } else if (
+            connectRequest.status >= 200 &&
+            connectRequest.status < 300
+        ) {
             if (editMode) {
                 buttonText = 'Save changes';
                 buttonClick = connect;
             } else {
                 buttonText = 'Connected';
             }
+        } else if (!connectRequest.status) {
+            buttonText = 'Connect';
+            buttonClick = connect;
         }
-
        return (
             <div className={styles.connectButtonContainer}>
                 <div
