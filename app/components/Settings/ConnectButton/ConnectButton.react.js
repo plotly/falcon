44ec1a821c61c7de8f32546bc830db70692ddc09
sobo <sobolevski.a@gmail.com>
2016-12-13 15:10:49 -0500

    PR comment about simplifying several R.has into pathOr

--- app/components/Settings/ConnectButton/ConnectButton.react.js
+++ app/components/Settings/ConnectButton/ConnectButton.react.js
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import * as styles from './ConnectButton.css';
-import {has} from 'ramda';
+import {has, pathOr} from 'ramda';
 
 const isLoading = (status) => status === 'loading';
 
@@ -23,19 +23,16 @@ export default class ConnectButton extends Component {
             buttonText = 'Connect';
             buttonClick = connect;
             // TODO - Try out locking the home folder and verifying this.
-            let errorMessage = 'Hm... had trouble connecting.';
-            if (
-               has('conent', connectRequest) &&
-               has('error', connectRequest.content)
-            ) {
-               errorMessage = connectRequest.content.error;
-            } else if (
-               has('content', saveConnectionsRequest) &&
-               has('error', saveConnectionsRequest.content) &&
-               has('message', saveConnectionsRequest.content.error)
-            ) {
-               errorMessage = saveConnectionsRequest.content.error.message;
-            }
+
+            // Possible errors.
+            const connectErrorMessage = pathOr(
+                null, ['content', 'error'], connectRequest
+            );
+            const saveErrorMessage = pathOr(
+                null, ['content', 'error', 'message'], saveConnectionsRequest
+            );
+            const genericErrorMessage = 'Hm... had trouble connecting.';
+            const errorMessage = connectErrorMessage || saveErrorMessage || genericErrorMessage;
             error = <div className={styles.errorMessage}>{errorMessage}</div>;
         } else if (
             isLoading(connectRequest.status) ||
