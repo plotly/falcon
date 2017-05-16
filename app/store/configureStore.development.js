ead63ece75852efa84c312fdd48498536c3c5811
chriddyp <chris@plot.ly>
2017-05-16 13:36:34 -0400

    indentation fix

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -25,10 +25,10 @@ export default function configureStore(initialState) {
     const store = createStore(rootReducer, initialState, enhancer);
 
     if (module.hot) {
-        module.hot.accept('../reducers', () =>
-        store.replaceReducer(require('../reducers'))
-    );
-}
+            module.hot.accept('../reducers', () =>
+            store.replaceReducer(require('../reducers'))
+        );
+    }
 
-return store;
+    return store;
 }
