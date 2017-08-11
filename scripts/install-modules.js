e735761f3460c1bf6d34c928bef057b3d953dffd
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-08-11 17:31:21 +0100

    ibmdb: Update scripts to rebuild native modules
    * Removed npm postinstall, because unit tests are run in node.
    
    * Added `npm run install:modules:node` to rebuild the native modules
      before running the unit tests.
    
    * Added `npm run install:modules:electron` to rebuild the native modules
      before building the app and running `npm start`.
    
    * Note that `script/install-modules.js` invokes the global `npm`. The
      local `npm` fails to build the native module `ibm_db`.

---
+++ scripts/install-modules.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const electronEnv = (process.argv[2] === '--electron');
+const winEnv = (process.platform === 'win32');
+
+function execSync(command) {
+    console.log('>>', command);
+    return require('child_process').execSync(command, {stdio: 'inherit'});
+}
+
+
+const globalBin = require('child_process').execSync('npm bin -g').toString().trim();
+const globalNpm = path.join(globalBin, 'npm');
+
+execSync(`${globalNpm} uninstall --no-save ibm_db`);
+execSync(`${globalNpm} install ibm_db`);
+
+if (electronEnv) {
+    const root = require('child_process').execSync('npm root').toString().trim();
+    const ibmdbInstaller = path.join(root, 'ibm_db', 'installer');
+    if (!winEnv) execSync(`chmod -R +w ${ibmdbInstaller}`);
+
+    const localBin = require('child_process').execSync('npm bin').toString().trim();
+    const installAppDeps = path.join(localBin, 'install-app-deps');
+    execSync(installAppDeps);
+}
