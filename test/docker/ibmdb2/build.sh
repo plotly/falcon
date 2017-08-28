52af4a0fe6a6613200e1b8d034622e3077838f06
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-08-28 21:12:02 +0100

    ibmdb: Update docker image
    * Skip header when importing the CSV file.

--- test/docker/ibmdb2/build.sh
+++ test/docker/ibmdb2/build.sh
@@ -21,27 +21,27 @@ ${DB_ADMIN})
 
         db2 create database plotly
 
-	# Grant CONNECT privilege on database user ${DB_USER}
-	db2 connect to plotly
+        # Grant CONNECT privilege on database user ${DB_USER}
+        db2 connect to plotly
 
         db2 "CREATE TABLE alcohol_consumption_by_country_2010 (Location nvarchar(64), Alcohol double)"
-        db2 import from /2010_alcohol_consumption_by_country.csv of del insert into alcohol_consumption_by_country_2010 || true
+        db2 import from /2010_alcohol_consumption_by_country.csv of del skipcount 1 insert into alcohol_consumption_by_country_2010 || true
 
-	echo db2 grant connect on database to user ${DB_USER}
-	db2 grant connect on database to user ${DB_USER}
+        echo db2 grant connect on database to user ${DB_USER}
+        db2 grant connect on database to user ${DB_USER}
 
-	db2 connect to plotly
-	echo db2 grant select on ${DB_ADMIN}.alcohol_consumption_by_country_2010 to user ${DB_USER}
-	db2 grant select on ${DB_ADMIN}.alcohol_consumption_by_country_2010 to user ${DB_USER}
+        db2 connect to plotly
+        echo db2 grant select on ${DB_ADMIN}.alcohol_consumption_by_country_2010 to user ${DB_USER}
+        db2 grant select on ${DB_ADMIN}.alcohol_consumption_by_country_2010 to user ${DB_USER}
 
-	# Stop database instance
-	db2 force applications all
-	db2 terminate
-	db2stop
+        # Stop database instance
+        db2 force applications all
+        db2 terminate
+        db2stop
 
-	;;
+        ;;
 
 *)
-	echo "error: '$0' must be invoked by root"
+        echo "error: '$0' must be invoked by root"
 
 esac
