79d5b9197702800405d775896581a268dcd7b68d
chriddyp <chris@plot.ly>
2017-09-08 22:16:35 -0400

    remove uglify to speed up builds

--- webpack.config.web.js
+++ webpack.config.web.js
@@ -35,12 +35,6 @@ const config = {
             __DEV__: false,
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                screw_ie8: true,
-                warnings: false
-            }
-        }),
         new webpack.LoaderOptionsPlugin({
             minimize: true
         })
