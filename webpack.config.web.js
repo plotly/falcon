7acf258a563298d84bb5b041c9030391ed3f0cf8
chriddyp <chris@plot.ly>
2017-09-08 20:21:49 -0400

    loaders -> rules

--- webpack.config.web.js
+++ webpack.config.web.js
@@ -25,7 +25,7 @@ const config = {
         ...baseConfig.module,
 
         rules: [
-            ...baseConfig.module.loaders,
+            ...baseConfig.module.rules,
 
             {
                 test: /\.global\.css$/,
