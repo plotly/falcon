18435ca53e0fed71b2546fc75f6448221e88007a
tarzzz <tarun.gaba7@gmail.com>
2017-09-13 15:53:51 -0400

    Added PlotlyOAuth plugin...  which will take care of authorization process.

---
+++ backend/plugins/authorization.js
@@ -0,0 +1,58 @@
+var errors = require('restify-errors');
+
+import {getSetting} from '../settings.js';
+import {generateAndSaveAccessToken} from '../utils/authUtils';
+import fetch from 'node-fetch';
+
+const ESCAPED_ROUTES = [
+  new RegExp('^/$'),
+  new RegExp('^/login$'),
+  new RegExp('^/static/'),
+  new RegExp('^/oauth2')
+]
+
+function accessTokenIsValid(access_token) {
+    return getSetting('ACCESS_TOKEN') === access_token;
+}
+
+export function PlotlyOAuth(options) {
+
+    function isAuthorized(req, res, next) {
+        let path = req.href();
+
+        // Auth is disabled for certain urls:
+        if ( ESCAPED_ROUTES.some(path.match.bind(path)) ) {
+          return (next());
+        }
+
+        if (!accessTokenIsValid(req.cookies['db-connector-auth-token'])) {
+
+            if (!req.cookies['plotly-auth-token']) {
+              return (next(new errors.InvalidCredentialsError('Please login to access this page.')));
+            }
+
+            const plotly_access_token = req.cookies['plotly-auth-token'];
+
+            fetch(`${getSetting('PLOTLY_API_URL')}/v2/users/current`, {
+                headers: {'Authorization': `Bearer ${plotly_access_token}`}
+            })
+            .then(userRes => userRes.json().then(userMeta => {
+
+              if ( userRes.status !== 200 ) {
+                return (next(new errors.InvalidCredentialsError('Please login to access this page.')));
+              }
+              else {
+                  const db_connector_access_token = generateAndSaveAccessToken();
+                  res.setCookie('db-connector-auth-token', db_connector_access_token, {'maxAge': 300, 'path': '/'});
+                  return (next());
+              }
+
+            }));
+
+        }
+
+        return (next());
+    }
+
+    return (isAuthorized);
+}
