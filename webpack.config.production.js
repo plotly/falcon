63a1242cda4b6bef1056348e77b00e6c1e15db2c
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-09-18 13:34:00 +0100

    webpack: remove uglify to speed up builds

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -15,26 +15,15 @@ const config = {
         publicPath: '../dist/'
     },
 
-    module: {
-        ...baseConfig.module,
-
-        loaders: [
-            ...baseConfig.module.rules,
-        ]
-    },
-
     plugins: [
         ...baseConfig.plugins,
         new webpack.DefinePlugin({
             __DEV__: false,
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                screw_ie8: true,
-                warnings: false
-            }
-        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
     ],
 
     target: 'electron-renderer'
