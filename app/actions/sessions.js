254c327f9b06f0f6566877090368dbb75c92157a
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-09-19 14:17:06 +0100

    app: use sql-schemas entrypoint on the backend

--- app/actions/sessions.js
+++ app/actions/sessions.js
@@ -222,15 +222,11 @@ export function previewTable (connectionId, dialect, table, database) {
 }
 
 export function getSqlSchema (connectionId, dialect, database) {
-    const body = {
-        query: DATABASE_SCHEMA(dialect, database)
-    };
     return apiThunk(
-        `connections/${connectionId}/query`,
+        `connections/${connectionId}/sql-schemas`,
         'POST',
         'schemaRequests',
-        connectionId,
-        body
+        connectionId
     );
 }
 
@@ -386,27 +382,5 @@ function PREVIEW_QUERY (dialect, table, database = '') {
             });
         default:
             throw new Error(`Dialect ${dialect} is not one of the DIALECTS`);
-    }
-}
-
-function DATABASE_SCHEMA (dialect, database = ''){
-    switch (dialect) {
-        case DIALECTS.IBM_DB2:
-            return `SELECT TABNAME, COLNAME, TYPENAME FROM syscat.columns WHERE SUBSTR(TABSCHEMA,1,3) != 'SYS';`
-        case DIALECTS.APACHE_SPARK:
-            return `show tables in ${database}`
-        case DIALECTS.MYSQL:
-        case DIALECTS.SQLITE:
-        case DIALECTS.MARIADB:
-            return `SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_schema = '${database}' ORDER BY table_name`;
-        case DIALECTS.POSTGRES:
-            return `SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_catalog = '${database}' AND table_schema = 'public' ORDER BY table_name`;
-        case DIALECTS.REDSHIFT:
-            return `SELECT tablename, "column", "type" FROM pg_table_def WHERE schemaname = 'public';`
-        case DIALECTS.MSSQL:
-            return `SELECT T.name AS Table_Name, C.name AS Column_Name, P.name AS Data_Type, P.max_length AS Size, CAST(P.precision AS VARCHAR) + '/' + CAST(P.scale AS VARCHAR) \
-AS Precision_Scale FROM sys.objects AS T JOIN sys.columns AS C ON T.object_id = C.object_id JOIN sys.types AS P ON C.system_type_id = P.system_type_id WHERE T.type_desc = 'USER_TABLE';`
-        default:
-            throw new Error(`Dialect ${dialect} is not one of the SQL DIALECTS`);
     }
 }
