eb9582f21feed9bafbcc2c2af2e5ef6daf6ebce3
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-09-22 14:25:05 +0100

    ibmdb: Upgrade to version 2.2.0

--- scripts/install-modules.js
+++ scripts/install-modules.js
@@ -1,22 +1,16 @@
 const path = require("path");
 
 const electronEnv = (process.argv[2] === '--electron');
-const winEnv = (process.platform === 'win32');
 
 function execSync(command) {
     console.log('>>', command);
     return require('child_process').execSync(command, {stdio: 'inherit'});
 }
 
-
-execSync('yarn remove ibm_db');
-execSync('yarn add ibm_db@2.0.0');
-
 if (electronEnv) {
-    const ibmdbInstaller = path.join(__dirname, '..', 'node_modules', 'ibm_db', 'installer');
-    if (!winEnv) execSync(`chmod -R +w ${ibmdbInstaller}`);
-
     const localBin = require('child_process').execSync('yarn bin').toString().trim();
     const installAppDeps = path.join(localBin, 'electron-builder install-app-deps');
     execSync(installAppDeps);
+} else {
+    execSync('yarn install --force');
 }
