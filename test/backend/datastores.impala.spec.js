7248f8e6ebd145149eac05989bd66a719ed3dd7f
tarzzz <tarun.gaba7@gmail.com>
2017-10-18 22:25:01 +0530

    Added tests.

---
+++ test/backend/datastores.impala.spec.js
@@ -0,0 +1,51 @@
+import {assert} from 'chai';
+
+import {DIALECTS} from '../../app/constants/constants.js';
+
+import {
+    query, connect, tables
+} from '../../backend/persistent/datastores/Datastores.js';
+
+
+const connection = {
+    dialect: DIALECTS.APACHE_IMPALA,
+    host: '35.184.155.127', // default: '127.0.0.1'
+    port: 21000,
+    database: 'plotly', // default: ''
+    timeout: 60
+};
+
+describe('Apache Spark:', function () {
+
+    it('connect succeeds', function() {
+        this.timeout(180 * 1000);
+        return connect(connection);
+    });
+
+    it('tables returns list of tables', function() {
+        return tables(connection).then(result => {
+            const tableName = (connection.database) ?
+                `${connection.database}.ALCOHOL_CONSUMPTION_BY_COUNTRY_2010`.toUpperCase() :
+                'ALCOHOL_CONSUMPTION_BY_COUNTRY_2010';
+
+            assert.deepEqual(result, [tableName]);
+        });
+    });
+
+    it('query returns rows and column names', function() {
+        const tableName = (connection.database) ?
+            `${connection.database}.ALCOHOL_CONSUMPTION_BY_COUNTRY_2010`.toUpperCase() :
+            'ALCOHOL_CONSUMPTION_BY_COUNTRY_2010';
+
+        return query(`SELECT * FROM ${tableName}\nLIMIT 5`, connection).then(results => {
+            assert.deepEqual(results.rows, [
+                ['Belarus', "17.5"],
+                ['Moldova', "16.8"],
+                ['Lithuania', "15.4"],
+                ['Russia', "15.1"],
+                ['Romania', "14.4"]
+            ]);
+            assert.deepEqual(results.columnnames, ['loc', 'alcohol']);
+        });
+    });
+});
