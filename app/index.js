a40af3e4e0569f78f4ddd37d030e94f61b516242
tarzzz <tarun.gaba7@gmail.com>
2017-10-31 11:11:28 +0530

    Use appropriate route prefix for on-prem

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {Route, Router, browserHistory} from 'react-router';
+import {Route, Router, browserHistory, useRouterHistory} from 'react-router';
 
 import configureStore from './store/configureStore';
 import {productName, version} from '../package.json';
@@ -9,6 +9,7 @@ import {productName, version} from '../package.json';
 import Login from './components/Login.react';
 import Configuration from './components/Configuration.react';
 import Status from './components/Oauth.react';
+import {homeUrl} from './utils/utils';
 
 const store = configureStore();
 
@@ -17,9 +18,9 @@ window.document.title = `${productName} v${version}`;
 render(
     <Provider store={store}>
       <Router history={browserHistory}>
-        <Route exact path="/" component={Configuration} />
-        <Route path="/login" component={Login} />
-        <Route path="/oauth2" component={Status} />
+        <Route path={homeUrl() + '/'} component={Configuration} />
+        <Route path={homeUrl() + '/login'} component={Login} />
+        <Route path={homeUrl() + '/oauth2'} component={Status} />
       </Router>
     </Provider>,
     document.getElementById('root')
