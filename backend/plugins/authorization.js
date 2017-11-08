873310ef9908b4e9db871deb57c464de60eb91c6
tarzzz <tarun.gaba7@gmail.com>
2017-11-08 11:35:05 +0530

    Debug2

--- backend/plugins/authorization.js
+++ backend/plugins/authorization.js
@@ -6,11 +6,11 @@ import Logger from '../logger';
 import fetch from 'node-fetch';
 
 const ESCAPED_ROUTES = [
-  new RegExp(`^${homeUrl()}/$`),
-  new RegExp(`^${homeUrl()}/login$`),
-  new RegExp(`^${homeUrl()}/static/`),
-  new RegExp(`^${homeUrl()}/oauth2$`),
-  new RegExp(`^${homeUrl()}/oauth2/callback$`)
+  new RegExp(`^/$`),
+  new RegExp(`^/login$`),
+  new RegExp(`^/static/`),
+  new RegExp(`^/oauth2$`),
+  new RegExp(`^/oauth2/callback$`)
 ];
 
 function accessTokenIsValid(access_token) {
