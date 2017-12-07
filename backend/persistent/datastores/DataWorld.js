1d18c4ea2cd15e6db72cf1e8dda09b14e37dd7d7
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2017-12-07 12:00:37 +0100

    Connect data.world

---
+++ backend/persistent/datastores/DataWorld.js
@@ -0,0 +1,54 @@
+import AWS from 'aws-sdk';
+import {parseCSV} from '../../parse.js';
+
+function createClient(connection) {
+    AWS.config.update({
+        accessKeyId: connection.accessKeyId,
+        secretAccessKey: connection.secretAccessKey
+    });
+    return new AWS.S3();
+}
+
+
+export function connect(connection) {
+    return new Promise((resolve, reject) => {
+      resolve({success: true});
+    });
+}
+
+/*
+ * Download a (csv) file from S3, parse it
+ * and return the results.
+ */
+export function query(key, connection) {
+    const {bucket} = connection;
+    const client = createClient(connection);
+    return new Promise((resolve, reject) => {
+        client.getObject({Bucket: bucket, Key: key}, (err, response) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            const textData = response.Body.toString();
+            // TODO ^ handle binary files too
+            parseCSV(textData).then(resolve);
+        });
+    });
+}
+
+/*
+ * List all of the files in an S3 bucket
+ */
+export function files(connection) {
+    const {bucket} = connection;
+    const client = createClient(connection);
+    return new Promise((resolve, reject) => {
+        client.listObjects({Bucket: bucket}, (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data.Contents);
+        });
+    });
+}
