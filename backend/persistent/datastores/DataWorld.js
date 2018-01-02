e0424ee00eded4d7350326e785783bbf36141064
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-01-02 15:13:19 +0300

    Use query to get all tables

--- backend/persistent/datastores/DataWorld.js
+++ backend/persistent/datastores/DataWorld.js
@@ -70,32 +70,22 @@ function getSchema(connection, csvFile) {
 
 export function schemas(connection) {
   return new Promise((resolve, reject) => {
-    fetch(`https://api.data.world/v0/datasets/${connection.owner}/${connection.identifier}`, {
-      method: 'GET',
-      headers: { 'Authorization': `Bearer ${connection.token}` }
-    })
-    .then(res => res.json())
-    .then(json => {
-      // json.code is defined only when there is an error
-      if (json.code) {
-        reject(json);
-      } else {
-        const promises = getCsvs(json.files).map(csvFile => {
-          return getSchema(connection, csvFile).then((schema) => {
-            return schema.fields.map((field) => {
-              return [schema.table, field.name, field.type];
-            });
+    query('SELECT * FROM Tables', connection).then((res) => {
+      const promises = res.rows.map((table) => {
+        return getSchema(connection, table[0]).then((schema) => {
+          return schema.fields.map((field) => {
+            return [schema.table, field.name, field.type];
           });
         });
-        Promise.all(promises).then((tableSchemas) => {
-          resolve({
-            columnNames: [ 'tablename', 'column_name', 'data_type' ],
-            rows: [].concat.apply([], tableSchemas)
-          });
-        });
-      }
-    });
+      });
 
+      Promise.all(promises).then((tableSchemas) => {
+        resolve({
+          columnNames: [ 'tablename', 'column_name', 'data_type' ],
+          rows: [].concat.apply([], tableSchemas)
+        });
+      });
+    });
   });
 }
 
