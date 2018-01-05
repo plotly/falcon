765474c4a181261971bd83ebaa201eb8e3844836
Shannon Lal <shannonlal@hotmail.com>
2018-01-04 22:59:01 -0500

    Check in version for getting connection status

--- backend/persistent/datastores/drivers/AWSAthenaDriver.js
+++ backend/persistent/datastores/drivers/AWSAthenaDriver.js
@@ -42,8 +42,6 @@ export function createAthenaClient( connection ){
  */
 export function startQuery( athenaClient, params ){
     let client = athenaClient;
-    let dbName = params.dbName;
-    let sqlStatement = params.sqlStatement;
 
     let queryParams = {
         QueryString: params.sqlStatement,
@@ -56,26 +54,92 @@ export function startQuery( athenaClient, params ){
         QueryExecutionContext: {
           Database: params.dbName
         }
-      };
+    };
     return new Promise(function(resolve, reject) {
 
         client.startQueryExecution( queryParams, (err, data) =>{
             if (err) {
                 reject( err );
             }else{
-                let queryId = data;
+                let queryId = data.QueryExecutionId;
                 resolve( queryId );
             }
           });
     });
 }
 
-export function queryResultsCompleted(){
+/**
+ * The following method will check to see if the query results 
+ * have completed.  It will return -1 if the query errored, 0 
+ * if it is still executing or 1 if it has completed
+ * @param {object} athenaClient  - Object created using create athena client
+ * @param {string} queryExecutionId 
+ * @param {int} -1 : Error, 0 : Still running, 1 : Completed
+ */
+export function queryResultsCompleted( athenaClient, queryExecutionId  ){
+    let client = athenaClient;
 
+    let queryParams = {
+        QueryExecutionId: queryExecutionId
+    };
+
+    return new Promise(function(resolve, reject) {
+
+        client.getQueryExecution( queryParams, (err, data) =>{
+            if (err) {
+                reject( -1 );
+            }else{
+
+                let state = data.QueryExecution.Status.State;
+                let queryState = 0;
+                switch (state) {
+                    case 'QUEUED':
+                        queryState = 0;
+                        break;
+                    case 'RUNNING':
+                        queryState = 0;
+                        break;
+                    case 'SUCCEEDED':
+                        queryState = 1;
+                        break;
+                    case 'FAILED':
+                        queryState = -1;
+                        break;
+                    case 'CANCELLED':
+                        queryState = -1;
+                        break;
+                    default:
+                        queryState = -1;
+                        break;
+                }
+                resolve( queryState );
+            }
+        });
+    });
 }
 
-export function cancelQuery(){
+/**
+ * The following method will stop the query execution based on the query id
+ * @param {object} athenaClient  - Object created using create athena client
+ * @param {string} queryExecutionId 
+ */
+export function stopQuery( athenaClient, queryExecutionId ){
+    let client = athenaClient;
 
+    let queryParams = {
+        QueryExecutionId: queryExecutionId
+    };
+
+    return new Promise(function(resolve, reject) {
+
+        client.stopQueryExecution( queryParams, (err, data) =>{
+            if (err) {
+                reject( err );
+            }else{
+                resolve( data);
+            }
+        });
+    });
 }
 
 export function getQueryResults(){
--- test/manual/AthenaClient.js
+++ test/manual/AthenaClient.js
@@ -1,17 +1,33 @@
 
 
-import {createAthenaClient, startQuery} from '../../backend/persistent/datastores/drivers/AWSAthenaDriver';
+import {createAthenaClient, startQuery, stopQuery, queryResultsCompleted} from '../../backend/persistent/datastores/drivers/AWSAthenaDriver';
 
 
 const client = createAthenaClient();
 
 let params  = {
     dbName :'shopscreen_logs',
-    sqlStatement: 'select * from clean_logs',
+    sqlStatement: 'select * from clean_logs limit 1000',
     s3Outputlocation: 's3://aws-athena-query-results-575576301786-us-east-1/'
 };
 startQuery( client, params ).then( rst =>{
     console.log( 'Got the query id back', rst);
+
+    queryResultsCompleted( client, rst ).then( queryState =>{
+        if( queryState === -1 ){
+            console.log( 'Error Querying ');
+        } else if( queryState === 0 ){
+            stopQuery( client, rst ).then( stopRst =>{
+                console.log( 'Query Stopped', stopRst);
+            });
+        }else{
+            console.log( 'Query has completed and is done');
+        }
+    });
+    //Stop the query
+    //stopQuery( client, rst ).then( stopRst =>{
+        //console.log( 'Query Stopped', stopRst);
+    //})//
 }).catch( err =>{
     console.log( 'err', err);
 });
