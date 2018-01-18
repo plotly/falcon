49b9eb3fd863e3711e7c166cf78c4e523b2a3255
Shannon Lal <shannonlal@hotmail.com>
2018-01-17 19:58:16 -0500

    Getting tests back up and running

---
+++ ATHENA_TODO.md
@@ -0,0 +1,5 @@
+1. Remove file before merging back into Master
+2. Get parameters from front end to pass to back end 
+3. Execute test to get schema
+4. Execute test to get tables
+5. Execute test to query results
--- backend/persistent/datastores/Athena.js
+++ backend/persistent/datastores/Athena.js
@@ -28,7 +28,6 @@ export function connect(connection) {
         region, accessKey, secretKey, dbName, sqlStatement, s3Outputlocation, queryTimeout
     } = connection;
 
-    /*
     //Validate that the connection has a 
     if( !region ){
         throw new Error(`The AWS Region was not defined`);
@@ -59,7 +58,7 @@ export function connect(connection) {
     }
     let con = {
         region, accessKey, secretKey, dbName, sqlStatement, s3Outputlocation, queryTimeout
-    };*/
+    };
 
     return new Promise(function(resolve, reject) {
         resolve( con );
@@ -121,7 +120,7 @@ export function schemas(connection){
     let columnnames = ['Table', 'column_name', 'data_type'];
     let rows = [];
 
-    /*connection.sqlStatement = `${SHOW_SCHEMA_QUERY} = '${connection.dbName}'` ;
+    connection.sqlStatement = `${SHOW_SCHEMA_QUERY} = '${connection.dbName}'` ;
     connection.queryTimeout = DEFAULT_QUERY_TIMEOUT;
     return new Promise(function(resolve, reject) {
         executeQuery( connection ).then( dataSet =>{
@@ -146,8 +145,8 @@ export function schemas(connection){
         }).catch( err =>{
             reject( err );
         });
-    });*/
-    for( let i=0; i< 4; i++){
+    });
+    /*for( let i=0; i< 4; i++){
 
         
         let r = [];
@@ -188,7 +187,7 @@ export function schemas(connection){
 
     return new Promise(function(resolve, reject) {
         resolve( {columnnames, rows} );
-    });
+    });*/
 }
 
 
@@ -204,7 +203,7 @@ export function schemas(connection){
  */
 export function tables(connection){
 
-    /*connection.sqlStatement = SHOW_TABLES_QUERY;
+    connection.sqlStatement = SHOW_TABLES_QUERY;
     connection.queryTimeout = DEFAULT_QUERY_TIMEOUT;
     return new Promise(function(resolve, reject) {
         executeQuery( connection ).then( dataSet =>{
@@ -222,12 +221,12 @@ export function tables(connection){
         }).catch( err =>{
             reject( err );
         });
-    });*/
+    });
 
-    let columnnames = [];
+    /*let columnnames = [];
     let rows = [];
     return new Promise(function(resolve, reject) {
         resolve( {columnnames, rows} );
-    });
+    });*/
 
 }
