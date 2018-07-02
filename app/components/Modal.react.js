cfc1919873c49efbd2049cf54f88e504fa2541b6
Mike Fix <mrfix84@gmail.com>
2018-07-02 09:03:32 -0700

    Review Comments #1:
    - Add comment
    - Remove old code
    - Add ms dev

--- app/components/Modal.react.js
+++ app/components/Modal.react.js
@@ -6,7 +6,6 @@ import enhanceWithClickOutside from 'react-click-outside';
 const Modal = props => {
   const EnhancedClass = enhanceWithClickOutside(class extends Component {
     handleClickOutside() {
-      // eslint-disable-next-line
       props.onClickAway();
     }
     render() {
@@ -14,27 +13,24 @@ const Modal = props => {
     }
   });
 
-  return props.open
-    ? (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '100vw',
-          height: '100vh',
-          margin: '0 auto',
-          position: 'fixed',
-          background: 'rgba(0, 0, 0, 0.2)',
-          top: 0,
-          left: 0,
-          zIndex: 9999
-        }}
-      >
-        <EnhancedClass {...props}>{props.children}</EnhancedClass>
-      </div>
-    )
-    : null;
+  return props.open ?
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100vw',
+        height: '100vh',
+        margin: '0 auto',
+        position: 'fixed',
+        background: 'rgba(0, 0, 0, 0.2)',
+        top: 0,
+        left: 0,
+        zIndex: 9999
+      }}
+    >
+      <EnhancedClass {...props}>{props.children}</EnhancedClass>
+    </div> : null;
 };
 
 Modal.propTypes = {
