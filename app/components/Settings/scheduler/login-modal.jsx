706c067e1c167f842a62763c3f059b38fd370f73
Mike Fix <mrfix84@gmail.com>
2018-07-05 11:54:28 -0700

    Extract login-modal styles to CSS file

--- app/components/Settings/scheduler/login-modal.jsx
+++ app/components/Settings/scheduler/login-modal.jsx
@@ -4,44 +4,30 @@ import PropTypes from 'prop-types';
 import { Row, Column } from '../../layout.jsx';
 import Modal from '../../modal.jsx';
 
-const styles = {
-    column: { width: '400px', background: '#F5F7FB', position: 'relative' },
-    header: { marginBottom: '16px', padding: '0 32px' },
-    button: {
-        position: 'absolute',
-        top: '16px',
-        right: '16px',
-        padding: '2px 4px'
-    },
-    detailsColumn: { padding: '0 32px' },
-    p: { margin: '56px 0 8px', padding: '16px' },
-    submit: {
-        width: '100%',
-        margin: '24px 32px 32px'
-    },
-    row: { justifyContent: 'flex-start' }
-};
+import './login-modal.css';
+
+const containerOverrideStyle = { width: '400px' };
 
 const PromptLoginModal = props => (
     <Modal
         open={props.open}
         onClickAway={props.onClickAway}
-        className="scheduler"
+        className="scheduler login-modal"
     >
-        <Column style={styles.column}>
-            <button onClick={props.onClickAway} style={styles.button}>
+        <Column style={containerOverrideStyle} className="container">
+            <button onClick={props.onClickAway} className="button">
                 &times;
             </button>
-            <Row style={styles.header}>
-                <p style={styles.p}>
+            <Row className="header">
+                <p>
                     To create a scheduled query, you'll need to be logged into
                     Plotly.
                 </p>
             </Row>
-            <Row style={{ width: '50%', marginLeft: '25%' }}>
+            <Row>
                 <button
                     type="submit"
-                    style={styles.submit}
+                    className="submit"
                     onClick={props.onSubmit}
                 >
                     Log In
