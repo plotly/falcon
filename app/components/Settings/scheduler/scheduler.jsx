14d11cb6df06829df2c49e8d0858e8e1f328eed5
Mike Fix <mrfix84@gmail.com>
2018-07-11 16:26:09 -0700

    Add pretty cron formatter for cron intervals

--- app/components/Settings/scheduler/scheduler.jsx
+++ app/components/Settings/scheduler/scheduler.jsx
@@ -5,6 +5,7 @@ import {contains} from 'ramda';
 import ReactDataGrid from 'react-data-grid';
 import ms from 'ms';
 import matchSorter from 'match-sorter';
+import * as prettyCron from 'prettycron';
 
 import CreateModal from './create-modal.jsx';
 import PreviewModal from './preview-modal.jsx';
@@ -48,12 +49,14 @@ class IntervalFormatter extends React.Component {
          * is an object containg the required `refreshInterval`.
          */
         value: PropTypes.shape({
-            refreshInterval: PropTypes.number.isRequired
+            refreshInterval: PropTypes.number.isRequired,
+            cronInterval: PropTypes.string
         })
     };
 
     render() {
         const run = this.props.value;
+
         return (
             <Row>
                 <Column>
@@ -62,9 +65,12 @@ class IntervalFormatter extends React.Component {
                             fontSize: 15
                         }}
                     >
-                        {`Runs every ${ms(run.refreshInterval * 1000, {
-                            long: true
-                        })}`}
+                        {run.cronInterval
+                          ? prettyCron.toString(run.cronInterval)
+                          : `Runs every ${ms(run.refreshInterval * 1000, {
+                              long: true
+                          })}`
+                        }
                     </em>
                 </Column>
             </Row>
--- webpack.config.web.js
+++ webpack.config.web.js
@@ -20,7 +20,9 @@ const config = {
 
     plugins: [
         ...baseConfig.plugins,
-
+        // This is to get `prettycron` to work in 'browser'. See this issue:
+        // https://github.com/bunkat/later/issues/155 for details.
+        new webpack.DefinePlugin({ 'process.env.LATER_COV': false }),
         new webpack.DefinePlugin({
             __DEV__: false,
             'process.env.NODE_ENV': JSON.stringify('production')
